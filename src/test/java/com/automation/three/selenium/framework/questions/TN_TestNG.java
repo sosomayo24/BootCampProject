a3c778011e407ecd34e41abd2695158bd2a289c2
sosomayo24 <sofianabassi24@gmail.com>
2023-12-11 05:05:51 -0500

    Your updated commit message here

--- src/test/java/com/automation/three/selenium/framework/questions/TN_TestNG.java
+++ src/test/java/com/automation/three/selenium/framework/questions/TN_TestNG.java
@@ -1,27 +1,32 @@
 package com.automation.three.selenium.framework.questions;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TN_TestNG {
 	
 	public WebDriver driver;
 	
-	@Test
+	@BeforeMethod
 	public void tutorialsNinja() {
 		
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://tutorialsninja.com/demo");
-		System.out.println("The title is : " + driver.getTitle());
-		
 		
 		
 	}
 	
 	@Test
 	public void verifyingTitle() {
+		
+		System.out.println("The title is : " + driver.getTitle());
 		
 		String actualTitle = driver.getTitle();
 		String expectedTitle = "Your Store";
@@ -31,5 +36,27 @@ public class TN_TestNG {
 		}
 		
 	}
+	
+	@Test 
+	public void login() {
+		
+		driver.findElement(By.linkText("My Account")).click();
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.id("input-email")).sendKeys("seleniumpanda@gmail.com");
+		driver.findElement(By.id("input-password")).sendKeys("Selenium@123");
+		driver.findElement(By.cssSelector("input.btn.btn-primary")).click();
+		WebElement logoutLink = driver.findElement(By.linkText("Logout"));
+		Assert.assertTrue(logoutLink.isEnabled());
+		
+		
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	
 
 }
